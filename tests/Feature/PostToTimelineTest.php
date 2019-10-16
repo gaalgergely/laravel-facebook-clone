45c3890ef64be9c0857c05ab67a19f278678737c
Victor Gonzalez <victor@coderstape.com>
2019-10-15 20:48:14 -0400

    asserting on the response

---
+++ tests/Feature/PostToTimelineTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Post;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PostToTimelineTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_post_a_text_post()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs($user = factory(User::class)->create(), 'api');
+
+        $response = $this->post('/api/posts', [
+            'data' => [
+                'type' => 'posts',
+                'attributes' => [
+                    'body' => 'Testing Body',
+                ]
+            ]
+        ]);
+
+        $post = Post::first();
+
+        $this->assertCount(1, Post::all());
+        $this->assertEquals($user->id, $post->user_id);
+        $this->assertEquals('Testing Body', $post->body);
+        $response->assertStatus(201)
+            ->assertJson([
+                'data' => [
+                    'type' => 'posts',
+                    'post_id' => $post->id,
+                    'attributes' => [
+                        'body' => 'Testing Body',
+                    ]
+                ],
+                'links' => [
+                    'self' => url('/posts/'.$post->id),
+                ]
+            ]);
+    }
+}
