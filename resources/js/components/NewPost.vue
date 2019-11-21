85f9e82b1736356e225b76db6c1aea100d170c5d
Victor Gonzalez <victor@coderstape.com>
2019-11-20 19:08:28 -0500

    submitting posts with images

--- resources/js/components/NewPost.vue
+++ resources/js/components/NewPost.vue
@@ -3,21 +3,28 @@
         <div class="flex justify-between items-center">
             <div>
                 <div class="w-8">
-                    <img :src="authUser.data.attributes.profile_image.data.attributes.path" alt="profile image for user" class="w-8 h-8 object-cover rounded-full">
+                    <img :src="authUser.data.attributes.profile_image.data.attributes.path"
+                         alt="profile image for user" class="w-8 h-8 object-cover rounded-full">
                 </div>
             </div>
             <div class="flex-1 flex mx-4">
-                <input v-model="postMessage" type="text" name="body" class="w-full pl-4 h-8 bg-gray-200 rounded-full focus:outline-none focus:shadow-outline text-sm" placeholder="Add a post">
+                <input v-model="postMessage" type="text" name="body"
+                       class="w-full pl-4 h-8 bg-gray-200 rounded-full focus:outline-none focus:shadow-outline text-sm"
+                       placeholder="Add a post">
                 <transition name="fade">
                     <button v-if="postMessage"
-                            @click="$store.dispatch('postMessage')"
+                            @click="postHandler"
                             class="bg-gray-200 ml-2 px-3 py-1 rounded-full">Post
                     </button>
                 </transition>
             </div>
             <div>
-                <button class="flex justify-center items-center rounded-full w-10 h-10 bg-gray-200">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-current w-5 h-5"><path d="M21.8 4H2.2c-.2 0-.3.2-.3.3v15.3c0 .3.1.4.3.4h19.6c.2 0 .3-.1.3-.3V4.3c0-.1-.1-.3-.3-.3zm-1.6 13.4l-4.4-4.6c0-.1-.1-.1-.2 0l-3.1 2.7-3.9-4.8h-.1s-.1 0-.1.1L3.8 17V6h16.4v11.4zm-4.9-6.8c.9 0 1.6-.7 1.6-1.6 0-.9-.7-1.6-1.6-1.6-.9 0-1.6.7-1.6 1.6.1.9.8 1.6 1.6 1.6z"/></svg>
+                <button ref="postImage" class="dz-clickable flex justify-center items-center rounded-full w-10 h-10 bg-gray-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                         class="dz-clickable fill-current w-5 h-5">
+                        <path
+                            d="M21.8 4H2.2c-.2 0-.3.2-.3.3v15.3c0 .3.1.4.3.4h19.6c.2 0 .3-.1.3-.3V4.3c0-.1-.1-.3-.3-.3zm-1.6 13.4l-4.4-4.6c0-.1-.1-.1-.2 0l-3.1 2.7-3.9-4.8h-.1s-.1 0-.1.1L3.8 17V6h16.4v11.4zm-4.9-6.8c.9 0 1.6-.7 1.6-1.6 0-.9-.7-1.6-1.6-1.6-.9 0-1.6.7-1.6 1.6.1.9.8 1.6 1.6 1.6z"/>
+                    </svg>
                 </button>
             </div>
         </div>
@@ -26,10 +33,21 @@
 
 <script>
     import _ from 'lodash';
-    import { mapGetters } from 'vuex';
+    import {mapGetters} from 'vuex';
+    import Dropzone from 'dropzone';
 
     export default {
         name: "NewPost",
+
+        data: () => {
+            return {
+                dropzone: null,
+            };
+        },
+
+        mounted() {
+            this.dropzone = new Dropzone(this.$refs.postImage, this.settings);
+        },
 
         computed: {
             ...mapGetters({
@@ -43,6 +61,40 @@
                 set: _.debounce(function (postMessage) {
                     this.$store.commit('updateMessage', postMessage);
                 }, 300),
+            },
+
+            settings() {
+                return {
+                    paramName: 'image',
+                    url: '/api/posts',
+                    acceptedFiles: 'image/*',
+                    clickable: '.dz-clickable',
+                    autoProcessQueue: false,
+                    params: {
+                        'width': 1000,
+                        'height': 1000,
+                    },
+                    headers: {
+                        'X-CSRF-TOKEN': document.head.querySelector('meta[name=csrf-token]').content,
+                    },
+                    sending: (file, xhr, formData) => {
+                        formData.append('body', this.$store.getters.postMessage);
+                    },
+                    success: (event, res) => {
+                        alert('success');
+                    }
+
+                };
+            },
+        },
+
+        methods: {
+            postHandler() {
+                if (this.dropzone.getAcceptedFiles().length) {
+                    this.dropzone.processQueue();
+                } else {
+                    this.$store.dispatch('postMessage');
+                }
             }
         }
     }
